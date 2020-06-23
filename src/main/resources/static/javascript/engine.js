f130976a7cd20212fb0817e545ba49758f258e66
Dimitrios Papazisis <gmonster1st@gmail.com>
2020-06-23 14:02:10 +0300

    Implemented clock, disable add button when form is active, added event listener to cancels button, implemented cancel form function, added 2 more TODOs

--- src/main/resources/static/javascript/engine.js
+++ src/main/resources/static/javascript/engine.js
@@ -1,3 +1,4 @@
+const clock = document.querySelector("#clock");
 const radioBtn = document.querySelectorAll("input[name=sort]");
 const addBtn = document.querySelector("#addBtn");
 const list = document.querySelector("ul");
@@ -8,9 +9,9 @@ const http = new XMLHttpRequest();
 window.onload = loadData();
 
 //#region Event handlers
-//TODO: Add eventListeners to submit and cancel buttons
+//TODO: Add eventListeners to submit button
 for (let i = 0; i < radioBtn.length; i++) {
-    radioBtn[i].addEventListener("click", loadData);
+  radioBtn[i].addEventListener("click", loadData);
 }
 
 addBtn.addEventListener("click", createAddForm);
@@ -20,29 +21,47 @@ addBtn.addEventListener("click", createAddForm);
  * Loads the data to Html document using spring boot api.
  */
 function loadData() {
-    console.debug("call loadData");
+  console.debug("call loadData");
 
-    while (list.lastElementChild) {
-        list.removeChild(list.lastElementChild);
+  startClock();
+
+  while (list.lastElementChild) {
+    list.removeChild(list.lastElementChild);
+  }
+
+  let command = this.value;
+  if (typeof command == "undefined") {
+    command = document.querySelector("input[name=sort]:checked").value;
+  }
+
+  let apiCmd = url + command;
+  console.debug(radioBtn.value);
+  console.debug(apiCmd);
+  http.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let myArr = JSON.parse(this.responseText);
+      console.debug(myArr);
+      fillList(myArr);
     }
+  };
+  http.open("GET", apiCmd, true);
+  http.send();
+}
+/**
+ * Implements the clock.
+ */
+function startClock() {
+  let now = new Date();
+  let hour = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
 
-    let command = this.value;
-    if (typeof command == "undefined") {
-        command = document.querySelector("input[name=sort]:checked").value;
-    }
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
 
-    let apiCmd = url + command;
-    console.debug(radioBtn.value);
-    console.debug(apiCmd);
-    http.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let myArr = JSON.parse(this.responseText);
-            console.debug(myArr);
-            fillList(myArr);
-        }
-    };
-    http.open("GET", apiCmd, true);
-    http.send();
+  clock.innerHTML = hour + ":" + min + ":" + sec;
+  setTimeout(startClock, 1000);
 }
 
 /**
@@ -50,10 +69,10 @@ function loadData() {
  * @param {Array<Object>} myArr - Array of objects to fill list elements
  */
 function fillList(myArr) {
-    console.debug("call fillList");
-    for (let i = 0; i < myArr.length; i++) {
-        list.appendChild(createListItem(myArr[i]));
-    }
+  console.debug("call fillList");
+  for (let i = 0; i < myArr.length; i++) {
+    list.appendChild(createListItem(myArr[i]));
+  }
 }
 
 /**
@@ -62,10 +81,11 @@ function fillList(myArr) {
  * @return {<li> element} li element with data.
  */
 function createListItem(content) {
-    console.debug("call createListItem");
-    let item = document.createElement("li");
-    item.textContent = toShortString(content);
-    return item;
+  console.debug("call createListItem");
+  let item = document.createElement("li");
+  item.textContent = toShortString(content);
+  // TODO: Add event listeners to list items for details
+  return item;
 }
 
 /**
@@ -74,16 +94,16 @@ function createListItem(content) {
  * @return {String} Returns a string.
  */
 function toShortString(obj) {
-    return (
-        "Id: " +
-        obj.id +
-        " " +
-        "Description: " +
-        obj.description +
-        " " +
-        "Due Date: " +
-        obj.dueDate
-    );
+  return (
+    "Id: " +
+    obj.id +
+    " " +
+    "Description: " +
+    obj.description +
+    " " +
+    "Due Date: " +
+    obj.dueDate
+  );
 }
 
 // TODO: add functionality to the field add labels and names
@@ -92,42 +112,43 @@ function toShortString(obj) {
  * Creates a form element with inputs.
  */
 function createAddForm() {
-    let addForm = createForm("addForm");
-    let desc = createInput("text", "desc");
-    let priority = createInput("text", "priority");
-    let dueDate = createInput("date", "dueDate");
-    dueDate.setAttribute("value", new Date().toJSON().split("T")[0]);
-    let alert = createInput("checkbox", "alert");
-    alert.setAttribute("value", true);
-    let daysBefore = createInput("text", "daysBefore");
-    let comments = document.createElement("textarea");
-    comments.setAttribute("id", "comments");
-    comments.setAttribute("placeHolder", "Your Comments Here...");
-    let completed = createInput("checkbox", "completed");
-    completed.setAttribute("value", true);
-    let add = createInput("button", "add", "Προσθήκη");
-    let cancel = createInput("button", "cancel", "Ακύρωση");
+  let addForm = createForm("addForm");
+  let desc = createInput("text", "desc");
+  let priority = createInput("text", "priority");
+  let dueDate = createInput("date", "dueDate");
+  dueDate.setAttribute("value", new Date().toJSON().split("T")[0]);
+  let alert = createInput("checkbox", "alert");
+  alert.setAttribute("value", true);
+  let daysBefore = createInput("text", "daysBefore");
+  let comments = document.createElement("textarea");
+  comments.setAttribute("id", "comments");
+  comments.setAttribute("placeHolder", "Your Comments Here...");
+  let completed = createInput("checkbox", "completed");
+  completed.setAttribute("value", true);
+  let add = createInput("button", "add", "Προσθήκη");
+  let cancel = createInput("button", "cancel", "Ακύρωση");
 
-    addForm.appendChild(createLabel("desc", "Περιγραφή:"));
-    addForm.appendChild(desc);
-    addForm.appendChild(createLabel("priority", "Προτεραιότητα:"));
-    addForm.appendChild(priority);
-    addForm.appendChild(createLabel("priority", "Ημερομηνία:"));
-    addForm.appendChild(dueDate);
-    addForm.appendChild(createLabel("priority", "Υπενθύμιση:"));
-    addForm.appendChild(alert);
-    addForm.appendChild(createLabel("priority", "'Υπενθύμιση' Ημέρες πρίν:"));
-    addForm.appendChild(daysBefore);
-    addForm.appendChild(createLabel("priority", "Σχόλια:"));
-    addForm.appendChild(comments);
-    addForm.appendChild(createLabel("priority", "Ολοκληρώθηκε:"));
-    addForm.appendChild(completed);
-    addForm.appendChild(add);
-    addForm.appendChild(cancel);
+  addForm.appendChild(createLabel("desc", "Περιγραφή:"));
+  addForm.appendChild(desc);
+  addForm.appendChild(createLabel("priority", "Προτεραιότητα:"));
+  addForm.appendChild(priority);
+  addForm.appendChild(createLabel("priority", "Ημερομηνία:"));
+  addForm.appendChild(dueDate);
+  addForm.appendChild(createLabel("priority", "Υπενθύμιση:"));
+  addForm.appendChild(alert);
+  addForm.appendChild(createLabel("priority", "'Υπενθύμιση' Ημέρες πρίν:"));
+  addForm.appendChild(daysBefore);
+  addForm.appendChild(createLabel("priority", "Σχόλια:"));
+  addForm.appendChild(comments);
+  addForm.appendChild(createLabel("priority", "Ολοκληρώθηκε:"));
+  addForm.appendChild(completed);
+  addForm.appendChild(add);
+  addForm.appendChild(cancel);
 
-    details.appendChild(addForm);
+  details.appendChild(addForm);
 
-    //TODO: Disable add button when addForm is active
+  //Disable add button when addForm is active
+  addBtn.disabled = true;
 }
 
 /**
@@ -136,9 +157,9 @@ function createAddForm() {
  * @return {<form> element} Returns a form element in the DOM with id attribute.
  */
 function createForm(id) {
-    let form = document.createElement("form");
-    form.setAttribute("id", id);
-    return form;
+  let form = document.createElement("form");
+  form.setAttribute("id", id);
+  return form;
 }
 
 /**
@@ -151,18 +172,21 @@ function createForm(id) {
  * @return {<input> element} Returns an input element in the DOM.
  */
 function createInput(type, id, text = "") {
-    console.debug("input attributes:", type, id);
-    let element = document.createElement("INPUT");
-    element.setAttribute("type", type);
-    element.setAttribute("id", id);
+  console.debug("input attributes:", type, id);
+  let element = document.createElement("INPUT");
+  element.setAttribute("type", type);
+  element.setAttribute("id", id);
 
-    //TODO: refactor to accept text content too for text fields
+  //TODO: refactor to accept text content too for text fields
 
-    if (type === "button") {
-        console.debug("text = ", text);
-        element.value = text;
+  if (type === "button") {
+    console.debug("text = ", text);
+    element.value = text;
+    if (id === "cancel") {
+      element.addEventListener("click", cancelForm);
     }
-    return element;
+  }
+  return element;
 }
 /**
  * Creates a label element in the DOM.
@@ -173,10 +197,18 @@ function createInput(type, id, text = "") {
  * @return {<label> element} Returns a label element.
  */
 function createLabel(id, text) {
-    let label = document.createElement("label");
-    label.setAttribute("for", id);
-    label.textContent = text;
-    return label;
+  let label = document.createElement("label");
+  label.setAttribute("for", id);
+  label.textContent = text;
+  return label;
 }
 
 //TODO: Add Edit function that pre fills the input elements with the values from the object selected
+
+/**
+ * Cancels form and removes form elements.
+ */
+function cancelForm() {
+  addForm.remove();
+  addBtn.disabled = false;
+}
